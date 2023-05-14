2c46a5853e08c62e8fdd0f2687075a3d16c2ecd4
AntonioMPereira <antoniofmpereira@gmail.com>
2023-05-14 10:41:46 +0100

    Add social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,8 +10,22 @@ class Footer extends Component {
     return (
       <div className="footer-component">
         <h2>{contacts.message}</h2>
-        <p><strong>Email:</strong> {contacts.email}</p>
+        <p className="footer-email"><strong>Email:</strong> {contacts.email}</p>
         <p><strong>Telemóvel:</strong> {contacts.phone}</p>
+        <div className="social-media-container">
+          <p>Segue-nos em:</p>
+          <a target="_blank" title="Follow us on Facebook" 
+            href="https://www.facebook.com/PLACEHOLDER">
+            <img alt="follow us on Facebook" 
+              src="https://marketing.create-cdn.net/assets/facebook40x40.png" />
+          </a>
+          <a target="_blank" title="Follow us on Instagram" 
+            href="https://www.instagram.com/PLACEHOLDER">
+            <img alt="follow us on instagram" 
+              src="https://marketing.create-cdn.net/assets/instagram40x40.png" />
+          </a>
+        </div>
+
       </div>
     );
   }
