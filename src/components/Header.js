198f60978d92b5a04154c233a9958010b12c924c
AntonioMPereira <antoniofmpereira@gmail.com>
2023-05-30 17:50:48 +0100

    Tag editing

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
     return (
       <div className="header-component">
         <h1 className="header-title">{headerTitle}</h1>
-        <h3 className="sub-header">Apoio Pedagógico | Preparação para Exames | Revisão Textual | Traduções</h3>
+        <h3 className="sub-header">Explicações | Preparação para Exames | Revisão Textual | Traduções</h3>
       </div>
     );
   }
